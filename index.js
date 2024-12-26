8b937d3e3da8fabd0f4896b3139284263f4d0b5e
jdu200027 <200027a@jdu.uz>
2024-12-26 23:18:19 +0500

    get values from post method

---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    const { statusCode } = res;
+    if (req.method === "GET" && statusCode === 200) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(`
+                </h1>Status</h4>
+                <form action="/" method="POST">
+                    <input name="username">
+                    <button type="submit">BTN</button> 
+                </form>
+            `)
+    } else if (req.method === "POST" && statusCode === 200 ) {
+        const result = [];
+        req.on('data', attr => {
+            result.push(Buffer.from(attr))
+        });
+
+        req.on('end', () => {
+            const message = result.toString()
+            res.end(
+                `
+                <p>Email is ${message}</p>
+            `
+            )
+        })
+    }
+})
+
+server.listen(5000, console.log("Server is running 5000"));
